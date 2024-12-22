34cce8716f76d7c4c6bb390478540cc0fa1a75b5
Daniel Gavidia <dgavidia1@gmail.com>
2024-12-22 08:50:58 -0500

    Added logic for moving from one image to the next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,17 @@
 import { useImages } from "@/hooks/hooks.images";
 
 const Page = () => {
-  const { images } = useImages();
-  console.log(images);
+  const { currentImageIndex, setCurrentImageIndex, currentImage } = useImages();
 
-  return <div>{JSON.stringify(images)}</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="text-2xl font-bold">Goblins Whiteboard Labeling Systems</p>
+      <div>
+        {currentImage && <img src={currentImage.url} alt={currentImage.id} className="h-96" />}
+      </div>
+      <button onClick={() => setCurrentImageIndex(currentImageIndex + 1)}>Next</button>
+    </div>
+  );
 };
 
 export default Page;
--- src/hooks/hooks.images.ts
+++ src/hooks/hooks.images.ts
@@ -4,10 +4,18 @@ import { getImages } from "@/utils/utils.get-images";
 
 export const useImages = () => {
   const [images, setImages] = useState<Image[]>([]);
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+  const currentImage = images[currentImageIndex];
 
+  // Fetch images from CSV
   useEffect(() => {
     getImages().then(setImages);
   }, []);
 
-  return { images, setImages };
+  // Set current image to first image in array
+  useEffect(() => {
+    setCurrentImageIndex(0);
+  }, [images]);
+
+  return { images, setImages, currentImageIndex, setCurrentImageIndex, currentImage };
 };
