1baf564caacb9a35cc9d33a92e07726840b62ea7
Daniel Gavidia <dgavidia1@gmail.com>
2024-12-22 10:44:22 -0500

    File deployment fix

--- src/utils/utils.get-images.ts
+++ src/utils/utils.get-images.ts
@@ -1,19 +1,16 @@
-"use server";
+// "use server";
 
-import { parse } from "csv-parse/sync";
-import fs from "fs/promises";
-import path from "path";
 import { Image } from "@/types/types.images";
+import { parse } from "csv-parse/sync";
 
 export const getImages = async (): Promise<Image[]> => {
-  const csvPath = path.join(process.cwd(), "src", "lib", "whiteboards.csv");
-  const fileContent = await fs.readFile(csvPath, "utf-8");
+  const response = await fetch("/whiteboards.csv");
+  const fileContent = await response.text();
   const records = parse(fileContent, {
     columns: true,
     skip_empty_lines: true,
   });
 
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   return records.map((record: any) => ({
     id: record.id,
     url: record.image_url,
