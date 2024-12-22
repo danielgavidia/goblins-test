ea229e6b0f7fa2886c2a048b72df2afa19e08989
Daniel Gavidia <dgavidia1@gmail.com>
2024-12-22 10:30:20 -0500

    Deployment fixes

--- src/utils/utils.export-csv.ts
+++ src/utils/utils.export-csv.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const exportToCsv = (data: any[], filename: string = "export.csv"): void => {
   if (data.length === 0) return;
 
--- src/utils/utils.get-images.ts
+++ src/utils/utils.get-images.ts
@@ -13,6 +13,7 @@ export const getImages = async (): Promise<Image[]> => {
     skip_empty_lines: true,
   });
 
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   return records.map((record: any) => ({
     id: record.id,
     url: record.image_url,
